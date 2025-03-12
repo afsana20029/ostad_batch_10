29e964da39cd5fb5ae1e3648f382d42154913254
USER <afsanaaktermim2792001@gmail.com>
2025-03-12 20:35:58 +0600

    ternary

---
+++ lib/module_2/class_2/ternary.dart
@@ -0,0 +1,11 @@
+main() {
+  var amount = 500;
+  String test;
+  String? test2;
+  print(test2);
+  amount >= 1000
+      ? print('Ami car e jabo')
+      : (amount >= 200)
+          ? print('Ami bike jabo')
+          : (amount >= 100 ? print('jabo') : print('jabo na'));
+}
