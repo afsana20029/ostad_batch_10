1ea65fee762d6cb9494e8833010ab91a1a677b4a
USER <afsanaaktermim2792001@gmail.com>
2025-04-06 23:39:41 +0600

    polymorphisim,encapsulation

---
+++ lib/live_test1/live_test1.dart
@@ -0,0 +1,34 @@
+void main() {
+  List<Map<String, dynamic>> students = [
+    {
+      "name": "Alice",
+      "scores": [85, 90, 78]
+    },
+    {
+      "name": "Bob",
+      "scores": [88, 76, 95]
+    },
+    {
+      "name": "Charlie",
+      "scores": [90, 92, 85]
+    },
+  ];
+
+  Map<String, double> averagesScores = {};
+
+  for (var student in students) {
+    String name = student['name'];
+    List<int> scores = List<int>.from(student['scores']);
+    double avg = scores.reduce((a, b) => a + b) / scores.length;
+    averagesScores[name] = double.parse(avg.toStringAsFixed(2));
+  }
+
+  var sortedEntries = averagesScores.entries.toList()
+    ..sort((a, b) => b.value.compareTo(a.value));
+
+  Map<String, double> studentAverages = {
+    for (var entry in sortedEntries) entry.key: entry.value
+  };
+
+  print(studentAverages);
+}
